8bfb6fe9d736c50f820867134f62ddca2bb2e7f9
q-uest <33920550+q-uest@users.noreply.github.com>
2021-04-11 00:03:01 +0530

    Update Bootstrap.php

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -120,7 +120,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../';
+	$application_folder = '..';
 
 /*
  *---------------------------------------------------------------
