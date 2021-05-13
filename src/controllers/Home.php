15f8e5cab116b98e03a1cf5144aa4700bc106c87
q-uest <33920550+q-uest@users.noreply.github.com>
2021-05-13 19:44:45 +0530

    Update Home.php

--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 	public function index() {
 
 
-		echo "\nBuild Test - 3 13/may/2021 18:50";
+		echo "\nBuild Test - 3 13/may/2021 19:45";
 
 		if($this->session->userdata('logged_in'))
 		{
